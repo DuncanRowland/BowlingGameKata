d32a9a0a847a4a24a1fca5d1f8ac339dda9a8d4d
DuncanRowland <duncan.a.rowland@gmail.com>
2016-11-24 22:55:27 +0000

    Refactored Game

--- BowlingGame/Game.swift
+++ BowlingGame/Game.swift
@@ -14,34 +14,34 @@ class Game {
     private var _ballId: Int = 0
     public var score: Int = 0
     
-    func isFirstBallOfFrame()->Bool { return _ballId%2 == 0 }
+    func isFirstBallOfFrame()->Bool { return (_ballId % 2) == 1 }
     func isStrike(_ pins: Int)->Bool { return pins == 10 }
     func isSpare(_ pins: Int)->Bool { return (pins + _lastPins) == 10 }
-    func isLastFrame()->Bool { return _ballId > 17 }
+    func isLastFrame()->Bool { return _ballId > 18 }
     
-    func roll(_ pins: Int) {
-        score += pins * _multiplier[0]
-        
+    func updateMultiplier(_ pins: Int) {
         if isFirstBallOfFrame() { //possible stike
             if isStrike(pins) && !isLastFrame() {
-                _multiplier[0]=_multiplier[1]+1
-                _multiplier[1]=2
+                _multiplier[0] = _multiplier[1] + 1
+                _multiplier[1] = 2
                 _ballId += 1
-            } else {
-                _multiplier[0]=_multiplier[1]
-                _multiplier[1]=1
+                return
             }
         } else { //not a possible stike (i.e. so a possible spare)
             if isSpare(pins) && !isLastFrame() {
-                _multiplier[0]=2
-                _multiplier[1]=1
-            } else {
-                _multiplier[0]=_multiplier[1]
-                _multiplier[1]=1
+                _multiplier[0] = 2
+                _multiplier[1] = 1
+                return
             }
         }
-        
+        _multiplier[0] = _multiplier[1]
+        _multiplier[1] = 1
+    }
+    
+    func roll(_ pins: Int) {
+        score += pins * _multiplier[0]
         _lastPins = pins
         _ballId += 1
+        updateMultiplier(pins)
     }
 }
